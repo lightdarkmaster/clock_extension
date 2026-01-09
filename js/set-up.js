111e54b3ae7dea626cab906b59e1cb396268b794
lightdarkmaster <christian.barbosa05222001@gmail.com>
2026-01-09 10:35:19 +0800

    update code

--- js/set-up.js
+++ js/set-up.js
@@ -71,5 +71,15 @@ function checkFirstTime(){
       }
 }
 
+//Added things..
+/**
+ * Retrieves the username stored in local storage and logs it to the console.
+ * @returns {undefined}
+ */
+function createLead(){
+    const test = localStorage.getItem("username");
+    console.log(test);
+}
+
 var element = document.getElementById("confirmLang");
 element.addEventListener("click", clickConfirmLang);
